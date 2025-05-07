c0427df5d32abf971c4a13142b6f8fb903d57ff3
CaiShangLin <shang_cai@appdet.com>
2025-05-07 17:46:17 +0800

    Lect12

---
+++ buildSrc/src/main/kotlin/Build.kt
@@ -0,0 +1,29 @@
+sealed class Build {
+    open val isMinifyEnabled = false
+    open val isDebuggable = false
+    open val applicationIdSuffix = ""
+    open val versionNameSuffix = ""
+    open val enableUnitTestCoverage = false
+
+    object Debug : Build() {
+        override val isMinifyEnabled = false
+        override val isDebuggable = true
+        override val applicationIdSuffix = ".debug"
+        override val versionNameSuffix = "-debug"
+        override val enableUnitTestCoverage = true
+    }
+
+    object ReleaseExternalQA : Build() {
+        override val isMinifyEnabled = false
+        override val isDebuggable = false
+        override val applicationIdSuffix = ".QA"
+        override val versionNameSuffix = "-releaseExternalQA"
+        override val enableUnitTestCoverage = true
+    }
+
+    object Release : Build() {
+        override val isMinifyEnabled = true
+        override val isDebuggable = false
+        override val enableUnitTestCoverage = true
+    }
+}
