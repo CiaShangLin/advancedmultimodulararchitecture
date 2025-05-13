40f5989886ed8b1b69611824361cf36ce3d0b43e
CaiShangLin <shang_cai@appdet.com>
2025-05-13 15:54:28 +0800

    Lect19

---
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -0,0 +1,16 @@
+import org.gradle.api.Project
+import java.io.FileInputStream
+import java.util.Properties
+
+
+private const val LOCAL_PROPERTIES_FILE_NAME = "dev_credentials"
+
+fun Project.getLocalProperty(propertyName: String): String {
+    val localProperties = Properties().apply {
+        val localPropertiesFile = rootProject.file(LOCAL_PROPERTIES_FILE_NAME)
+        if (localPropertiesFile.exists()) {
+            load(FileInputStream(localPropertiesFile))
+        }
+    }
+    return localProperties.getProperty(propertyName)
+}
