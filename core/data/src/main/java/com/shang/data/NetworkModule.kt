5ceb87cfc871af27a8f6361fa69c09d977fb987f
CaiShangLin <shang_cai@appdet.com>
2025-05-31 20:12:15 +0800

    Lect57

--- core/data/src/main/java/com/shang/data/NetworkModule.kt
+++ core/data/src/main/java/com/shang/data/NetworkModule.kt
@@ -8,7 +8,9 @@ import okhttp3.Call
 import okhttp3.Interceptor
 import okhttp3.OkHttp
 import okhttp3.OkHttpClient
+import java.util.Locale
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 import kotlin.math.log
 
@@ -16,40 +18,67 @@ import kotlin.math.log
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
-//    @Provides
-//    @Singleton
-//    fun providerHeaderIntercept():Interceptor{
-//        return HeaderIntercept()
-//    }
 
-  @Provides
-  @Singleton
-  fun providerOkhttpLoggerIntercept(): okhttp3.logging.HttpLoggingInterceptor {
-    val logging = okhttp3.logging.HttpLoggingInterceptor()
-    if (BuildConfig.DEBUG) {
-      logging.level = okhttp3.logging.HttpLoggingInterceptor.Level.BODY
-    } else {
-      logging.level = okhttp3.logging.HttpLoggingInterceptor.Level.NONE
+    @Provides
+    @Named("clientId")
+    fun providerClientId(): String {
+        return ""
     }
-    if (!BuildConfig.DEBUG){
-      logging.redactHeader(CLIENT_ID_HEADER)
-      logging.redactHeader(ACCEPT_HEADER)
+
+    @Provides
+    @Named("accessToken")
+    fun providerAccessToken(): () -> String? {
+        return { "" }
     }
-    return logging
-  }
+
+    @Provides
+    @Named("language")
+    fun providerLanguage(): () -> Locale {
+        return { Locale.ENGLISH }
+    }
 
 
-  @Provides
-  @Singleton
-  fun providerOkhttpCallFactory(interceptor: Interceptor): Call.Factory {
-    return OkHttpClient.Builder()
-      .addInterceptor(interceptor)
-      .retryOnConnectionFailure(true)
-      .followRedirects(false)
-      .followSslRedirects(false)
-      .readTimeout(60,TimeUnit.SECONDS)
-      .writeTimeout(60,TimeUnit.SECONDS)
-      .connectTimeout(60,TimeUnit.SECONDS)
-      .build()
-  }
+    @Provides
+    @Singleton
+    @Named("HeaderIntercept")
+    fun providerHeaderIntercept(
+        @Named("clientId") clientId: String,
+        @Named("accessToken") accessToken: () -> String?,
+        @Named("language") language: () -> Locale
+    ): Interceptor {
+        return HeaderIntercept(clientId, accessToken, language)
+    }
+
+    @Provides
+    @Singleton
+    @Named("OkhttpLoggerIntercept")
+    fun providerOkhttpLoggerIntercept(): okhttp3.logging.HttpLoggingInterceptor {
+        val logging = okhttp3.logging.HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            logging.level = okhttp3.logging.HttpLoggingInterceptor.Level.BODY
+        } else {
+            logging.level = okhttp3.logging.HttpLoggingInterceptor.Level.NONE
+        }
+        if (!BuildConfig.DEBUG) {
+            logging.redactHeader(CLIENT_ID_HEADER)
+            logging.redactHeader(ACCEPT_HEADER)
+        }
+        return logging
+    }
+
+
+    @Provides
+    @Singleton
+    @Named("OkhttpCallFactory")
+    fun providerOkhttpCallFactory(interceptor: Interceptor): Call.Factory {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .retryOnConnectionFailure(true)
+            .followRedirects(false)
+            .followSslRedirects(false)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
+    }
 }
