f3a5edd10c46810f09bedd0d1b27e2e67b03952a
CiaShangLin <nick034po@yahoo.com.tw>
2025-06-01 13:22:29 +0800

    Lect60

--- core/data/src/release/kotlin/com/shang/data/OkhttpClientProvider.kt
+++ core/data/src/release/kotlin/com/shang/data/OkhttpClientProvider.kt
@@ -1,5 +1,6 @@
 package com.shang.data
 
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.Dispatcher
 import java.security.cert.X509Certificate
@@ -13,42 +14,12 @@ class OkHttpClientProvider : OkhttpClientProviderInterface{
     private var dispatcher = Dispatcher()
 
     override fun getOkHttpClient(pin: String): OkHttpClient.Builder {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+        val certificatePinner = CertificatePinner.Builder().add("*.yourdomain.com", pin).build()
 
-                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-
-                @Throws(CertificateException::class)
-                override fun checkClientTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String,
-                ) {
-                }
-
-                @Throws(CertificateException::class)
-                override fun checkServerTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String,
-                ) {
-                }
-            })
-
-            // Install the all-trusting trust manager
-            val sslContext = SSLContext.getInstance("TLS")
-            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-
-            val builder = OkHttpClient.Builder()
-            builder.dispatcher(dispatcher)
-            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
-            builder.hostnameVerifier(HostnameVerifier { _, _ -> true })
-            return builder
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
+        val builder = OkHttpClient.Builder()
+        builder.dispatcher(dispatcher)
+        builder.certificatePinner(certificatePinner)
+        return builder
     }
 
     override fun cancelAllRequest() {
