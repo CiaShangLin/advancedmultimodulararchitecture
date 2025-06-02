6c1d08d5c558a4768450a774f65bdacba87abec5
CaiShangLin <shang_cai@appdet.com>
2025-06-02 15:39:08 +0800

    Lect70

---
+++ core/data/src/main/java/com/shang/data/source/NetworkDataSource.kt
@@ -0,0 +1,13 @@
+package com.shang.data.source
+
+import com.google.gson.Gson
+import com.shang.data.connectivity.NetworkMonitorInterface
+
+class NetworkDataSource<SERVICE>(
+    val service: SERVICE,
+    val gson: Gson,
+    val networkMonitor: NetworkMonitorInterface,
+    val userIdProvider: () -> String
+) {
+
+}
