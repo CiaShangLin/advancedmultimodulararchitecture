3f7edd94d872e5a391cd54e6faaf4808d838b2d9
CaiShangLin <nick034po@yahoo.com.tw>
2025-06-04 15:59:05 +0800

    LECT90

---
+++ core/datastore/src/main/java/com/shang/datastore/AppSettings.kt
@@ -0,0 +1,20 @@
+package com.shang.datastore
+
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AppSettings(
+    val language: Language = Language.ENGLISH,
+    val lastKnownLocation: PersistentList<Location> = persistentListOf(),
+)
+
+@Serializable
+data class Location(val lat: Double, val long: Double)
+
+enum class Language {
+    ENGLISH,
+    ARABIC,
+    SPANISH,
+}
