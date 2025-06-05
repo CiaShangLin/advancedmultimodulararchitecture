8c6d7bc3deeea1ec8c6942791ca8ba5033e0c54a
CaiShangLin <nick034po@yahoo.com.tw>
2025-06-05 16:52:52 +0800

    LECT104

---
+++ core/protodatastore/src/main/java/com/shang/protodatastore/manager/SessionDataStoreInterface.kt
@@ -0,0 +1,30 @@
+package com.shang.protodatastore.manager
+
+
+import com.shang.protodatastore.Session
+import kotlinx.coroutines.flow.Flow
+
+
+interface SessionDataStoreInterface {
+    // setter functions
+    // setter functions
+
+    suspend fun setAccessToken(accessToken: String)
+    suspend fun setRefreshToken(refreshToken: String)
+    suspend fun setUserIdToken(userId: String)
+
+    suspend fun setSession(accessToken: String, refreshToken: String, userId: String)
+
+    // getters
+
+    suspend fun getAccessToken(): String
+    fun getAccessTokenFlow(): Flow<String>
+    suspend fun getRefreshToken(): String
+    fun getRefreshTokenFlow(): Flow<String>
+    suspend fun getUserId(): String
+    fun getUserIdFlow(): Flow<String>
+
+    suspend fun getSession(): Session
+    fun getSessionFlow(): Flow<Session>
+
+}
