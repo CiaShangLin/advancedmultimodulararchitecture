2fa2e9f76f0307bcabda06e7304394038cd40085
CaiShangLin <nick034po@yahoo.com.tw>
2025-06-05 18:13:07 +0800

    LECT106

--- app/src/main/java/com/minafarid/advancedmultimodulararchitecture/MainActivity.kt
+++ app/src/main/java/com/minafarid/advancedmultimodulararchitecture/MainActivity.kt
@@ -9,13 +9,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,10 +32,22 @@ import com.shang.datastore.AppSettings
 import com.shang.datastore.AppSettingsSerializer
 import com.shang.datastore.Language
 import com.shang.datastore.Location
+import com.shang.protodatastore.manager.preferences.PreferencesDataStoreInterface
+import com.shang.protodatastore.manager.session.SessionDataStoreInterface
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var sessionDataStoreInterface: SessionDataStoreInterface
+
+    @Inject
+    lateinit var preferencesDataStoreInterface: PreferencesDataStoreInterface
+
     lateinit var appSettingsDataStore: DataStore<AppSettings>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,7 +70,12 @@ class MainActivity : ComponentActivity() {
 //                        mapId = MapProvider.mapId,
 //                        modifier = Modifier.padding(innerPadding),
 //                    )
-                    SettingScreen(appSettingsDataStore, modifier = Modifier.padding(innerPadding))
+                    SettingScreen(
+                        appSettingsDataStore,
+                        sessionDataStoreInterface,
+                        preferencesDataStoreInterface,
+                        modifier = Modifier.padding(innerPadding),
+                    )
                 }
             }
         }
@@ -61,11 +83,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SettingScreen(appSettingsDataStore: DataStore<AppSettings>, modifier: Modifier) {
+fun SettingScreen(
+    appSettingsDataStore: DataStore<AppSettings>,
+    sessionDataStoreInterface: SessionDataStoreInterface,
+    preferencesDataStoreInterface: PreferencesDataStoreInterface,
+    modifier: Modifier,
+) {
     val scope = rememberCoroutineScope()
     val appSetting by appSettingsDataStore.data.collectAsState(initial = AppSettings())
 
+    val accessTokenFlow by sessionDataStoreInterface.getAccessTokenFlow().collectAsState("")
+    val accessTokenValue = remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            accessTokenValue.value = sessionDataStoreInterface.getAccessToken()
+        }
+    }
+
     Column(modifier = modifier) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "accessTokenFlow : $accessTokenFlow")
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "accessTokenValue : ${accessTokenValue.value}")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            scope.launch {
+                val newAccessToken = "newAccessToken : ${System.currentTimeMillis()}"
+                sessionDataStoreInterface.setAccessToken(newAccessToken)
+                accessTokenValue.value = (newAccessToken)
+            }
+        }) {
+            Text("insert")
+        }
+
         Text(text = "language : ${appSetting.language}")
         Text(text = "last location")
         appSetting.lastKnownLocation.forEach {
